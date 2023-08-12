dcd5ec3bca1a2a691c29695ca9cbd83da22f0e5e
Júlio Simas <123327767+juliofsimas@users.noreply.github.com>
2023-08-11 21:56:42 -0300

    Update RPS.py
    Finishing the game and testing

--- RPS.py
+++ RPS.py
@@ -6,10 +6,27 @@ computer= t[randint(0,2)]
 
 player=False
 
-while player == False
+while player == False:
     player = input ('Rock,Paper,Scissors? ')
     if player == computer:
         print ('Tie!')
     elif player == "Rock":
         if computer == "Paper":
-            print("You lose",computer,"covers",player)    
+            print("You lose",computer,"covers",player)
+        else:
+            print("You win!",player,"Smashes",computer)
+    elif player == "Paper":
+        if computer == "Scissors":
+            print("You lose...",computer,"cut",player)
+        else:
+            print("You win!",player,"covers",computer)
+    elif player == "Scissors":
+        if computer == "Rock":
+            print("You lose!",computer,"Smashes",player)
+        else:
+            print("You Win!",player,"cut",computer) 
+    else:
+        print("Thats not a valid play. Check your spelling")
+
+    player: False
+    computer=t[randint(0,2)]    
