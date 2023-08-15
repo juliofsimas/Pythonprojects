56e123b3ff6b22b8cc6913e6f15a6e4c8dcbd3f9
Júlio Simas <123327767+juliofsimas@users.noreply.github.com>
2023-08-14 22:09:40 -0300

    Update Hangman.py
    Creating looping and finishing the game

--- Hangman.py
+++ Hangman.py
@@ -1,4 +1,5 @@
 
+
 #definindo a função e criando as variaveis 
 def hangman(world):
     wrong = 0
@@ -14,6 +15,34 @@ def hangman(world):
     board = ["__"] * len(world)
     win = False
     print("Welcome to Hangman")
+
+    while wrong < len(world) - 1:
+        print ("\n")
+        msg = ("Guess a letter: ")
+        char = input(msg)
+
+        if char in rletters:
+            cind = rletters.index(char)
+            board [cind] = char
+            rletters [cind] = '$'
+        else:
+            wrong =+ 1
+
+        print(" ".join(board))
+        e = wrong + 1
+        print("\n".join[0:e]) 
+
+        if "__" not in board:
+            print ("You win!The word was: {}".format(world))
+            win = True
+            break
+
+    if not win:  
+        print("\n".join(stages[0:wrong]))
+        print("You lose! The word was: {}".format(world)) 
+
+hangman("Heart")       
+
 
 
     
