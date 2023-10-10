69eb205d5bba760ef5d32ac0e3767973718a6ca4
Júlio Simas <123327767+juliofsimas@users.noreply.github.com>
2023-10-09 22:02:44 -0300

    Update Pokemongenerator.py
    improving the if else method

--- Pokemongenerator.py
+++ Pokemongenerator.py
@@ -1,31 +1,56 @@
-#creating lists of region from pokemons
+#creating regions list
 
-regions = ['kanto','Jhoto','Hoen','Sinnoh','Unova']
+regions = ['Kanto','Jhoto','Hoen','Sinnoh','Unova']
+
+#Adding possibles choices for each region
+
 kantochoices = ['charmander', 'squirtle', 'bulbasaur']
 jhotochoices = ['cyndaquil','totodile','chicorita']
 hoenchoices = ['torchick','mudikip','treeko']
 sinnohchoices = ['chinchar','piplup','turtwig']
 
-#defining the lists of pokemons and atacks
+#listing attacks for each type
 types = ['fire', 'water', 'grass']
 fireskills = ['ember', 'flamethrower', 'fire blast', 'fire punch', 'flame wheel', 'fire spin', 'heat wave', 'blast burn', 'fire fang', 'eruption']
 waterskills = ['watergun', 'surf', 'hydro pump', 'aqua tail', 'bubble beam', 'water pulse', 'scald', 'dive', 'aqua jet', 'brine']
 grassskills = ['razor leaf', 'solar beam', 'vine whip', 'bullet seed', 'leaf blade', 'energy ball', 'petal dance', 'giga drain', 'grass knot', 'seed bomb']
 
 #startin survey
-regionchoice = input("Please, tell where you wish to begin you journey? ")
-if regionchoice not in regions:
-    print ('We cant provide you any choice. can you tell me another regio? chose one of these{}'.format(regions))
+regions = ['Kanto', 'Jhoto', 'Hoen', 'Sinnoh']
+
+while True:
+    regionchoice = input("PLease, what region do you wish to star you journey: ")
+    if regionchoice in regions:
+        print('great! You choose the region {}.'.format(regionchoice))
+        break  # Sai do loop quando uma escolha válida é feita
+    else:
+        print('Sorry, We cant provide any information about this place. Please, choose one of these: {}'.format(regions))
+
+
+#add options
+choices = []
+
+if regionchoice == 'Kanto':
+    choices = kantochoices
+elif regionchoice == 'Jhoto':
+    choices = jhotochoices
+elif regionchoice == 'Hoen':
+    choices = hoenchoices
+elif regionchoice == 'Sinnoh':
+    choices = sinnohchoices 
 else:
-    print ('oh! great place')
+    print('Something its wrong!')  
+           
   
-pokemon = input("Please choose your initial pokemon: ")
+pokemon = input("Please choose your initial pokemon: {}.".format(choices))
 
 #validation
-if pokemon not in choices:
-    print('The pokemon {} is not available for selection. Choose from {}'.format(pokemon, choices))
-else:
-    print('You chose well!')
+while True:
+  if pokemon not in choices:
+      print('The pokemon {} is not available for selection. Choose from {}'.format(pokemon, choices))
+      break
+  else:
+      print('You chose well!{} is a great pokemon'.format(pokemon)
 
 type = input("What Type of pokemon did you choose? ")
 
